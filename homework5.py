b4bd6f8d1a53e435b60ad20d3cc61687b15a3194
Ксения Че <chechulina75@list.ru>
2024-07-16 23:30:49 +0500

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+mmutable_var = (1, 'привет', True)
+tuple=mmutable_var
+print(tuple)
+tuple[0]=2
+mutable_list = [60, "спектр", 2, False]
+print(mutable_list)
+mutable_list[0] = 'apple',
+mutable_list[1] = 2
+mutable_list[2] = False
+print(mutable_list)
